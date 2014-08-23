13f78cd87deee426593281be4a3b8b2b7a00f975
Dan Schultzer <dan@dreamconception.com>
2014-08-23 17:28:40 -0500

    Add tests for Myth version >= 1.0.0

--- test/test_myth.rb
+++ test/test_myth.rb
@@ -15,6 +15,36 @@ TestCase = if defined? Minitest::Test
 require 'myth'
 require 'stringio'
 
+if !(version = Gem.loaded_specs['myth-source'].version) or Gem::Version.new(version) >= Gem::Version.new('1.0.0')
+
+class TestMyth < TestCase
+  def test_preprocess
+    assert_match "a:hover {\n  color: rgb(133, 159, 94);\n}",
+      Myth.preprocess(":root {--green: #a6c776;}a:hover {color: color(var(--green) shade(20%));}")
+  end
+
+  def test_preprocess_with_io
+    io = StringIO.new(":root {--green: #a6c776;}a:hover {color: color(var(--green) shade(20%));}\n")
+    assert_match "a:hover {\n  color: rgb(133, 159, 94);\n}",
+      Myth.preprocess(io)
+  end
+
+  def test_preprocessing_error
+    begin
+      Myth.preprocess("invalid")
+      flunk
+    rescue Myth::Error => e
+      assert e
+    end
+  end
+
+  def assert_no_match(expected, actual)
+    assert !expected.match(actual)
+  end
+end
+
+else
+
 class TestMyth < TestCase
   def test_preprocess
     assert_match "\n\na:hover {\n  color: rgb(133, 159, 94);\n}",
@@ -39,4 +69,6 @@ class TestMyth < TestCase
   def assert_no_match(expected, actual)
     assert !expected.match(actual)
   end
+end
+
 end
